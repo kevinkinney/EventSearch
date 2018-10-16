c8ab8a07c5751136ffa2bfe365c05d88ff2e33d3
Kevin Kinney <kevin.p.kinney@gmail.com>
2018-10-16 13:36:42 -0500

    Initial Commit

---
+++ EventSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventSearch
+//
+//  Created by Kevin Kinney on 10/16/18.
+//  Copyright © 2018 Kevin Kinney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
